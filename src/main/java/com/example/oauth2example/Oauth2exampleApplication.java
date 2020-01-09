e7fdd97cbfc42fc0e9c8b2c0fabd7de098a1ecd3
Qiming Chen <qimingchen91129@gmail.com>
2020-01-09 11:52:30 -0500

    first commit

---
+++ src/main/java/com/example/oauth2example/Oauth2exampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauth2example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2exampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2exampleApplication.class, args);
+	}
+
+}
